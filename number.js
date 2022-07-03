4279b2787168e29e8bed5b8bd6207cb296fe1e95
Olga <107260030+gvendaline@users.noreply.github.com>
2022-07-03 22:03:38 +0200

    Add files via upload
    All number from 1 to 135
    Odd number from 1 to 135
    number and sum

---
+++ number.js
@@ -0,0 +1,27 @@
+/*number from 1 to 135*/
+/*let arr = [];
+for (let i = 1; i <= 135; i++) {
+        arr.push(i)
+    }
+console.log(arr);*/
+let i = 0;
+  while (i <= 134) {
+    
+    i++
+ console.log(i)
+};
+/*number odd from 1 to 135*/
+let j = 0;
+  while (j <= 134) {
+    j++;
+    if(j%2) console.log(j);
+
+};
+
+/*number from 1 to 135 and sum*/
+let s = 0; sum=0;
+  while (s <= 134) {
+    s++; sum+=s;
+    console.log("Number is:", s, "SUM=", sum);
+
+};
